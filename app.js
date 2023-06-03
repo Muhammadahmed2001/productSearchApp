7da281c6ef4ad55ebf73a0042245d7b2c9f76019
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-06-03 19:30:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+var imtiazSuperMart = {
+    bakery: {
+        breadAndBuns : {
+            name : "Bake Parlor Burger Bun 4's",
+            company : "Bake Parlor",
+            price : 95,
+
+        },
+        biscuits : {},
+        cake : {},
+        rusks : {},
+    },
+    fresh: {},
+    beverages : {},
+    frozen : {},
+    pharmacy : {},
+
+
+}
