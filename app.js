8e6558751397e674cf3f5b814ae196918545ccd3
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-06-04 23:10:54 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,10 +4,32 @@
 var imtiazSuperMart = {
     bakery: {
         breadAndBuns : {
-            name : "Bake Parlor Burger Bun 4's",
-            company : "Bake Parlor",
-            price : 95,
+            burgerBun : {
+                name : "Bake Parlor Burger Bun 4's",
+                company : "Bake Parlor",
+                price : 95,
 
+            },
+            breadBrad : {
+                name : "Bake Parlor Bread Bran",
+                company : "Bake Parlor",
+                price : 145
+            },
+            fruitBun : {
+                name : "Bake Parlor Fruit Bun",
+                company : "Bake Parlor",
+                price : 47
+            },
+            pitaBread : {
+                name : "Dawn Pita Bread 5's",
+                company : "Dawn",
+                price : 85
+            },
+            breadMilky : {
+                name : "Dawn Bread Milky",
+                company : "Dawn",
+                price : 105
+            }
         },
         biscuits : {},
         cake : {},
