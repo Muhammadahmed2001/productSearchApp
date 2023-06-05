ed56d351f1ff281ef337801e239c47886f567470
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-06-06 01:58:35 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -38,50 +38,285 @@ var onlineStotre = {
             }
         },
         biscuits: {
-            cocomo : {
-                name : "Bisconni Cocomo Pouch 130g",
-                company : "Bisconni",
-                price : 59,
-                ime : "",
+            cocomo: {
+                name: "Bisconni Cocomo Pouch 130g",
+                company: "Bisconni",
+                price: 59,
+                img: "pic/Biscuits/cocomo.webp",
             },
-            innovative : {
-                name : "Innovative Butter Crunch H/R Rs.20/- 6's Box",
-                company : "Innovative",
-                price : 105,
-                img : "",
+            innovative: {
+                name: "Innovative Butter Crunch H/R Rs.20/- 6's Box",
+                company: "Innovative",
+                price: 105,
+                img: "pic/Biscuits/innovative.webp",
             },
-            gala : {
-                name : "LU Gala Egg S/P 6's Box",
-                company : "LU",
-                price : 159,
-                img : "",
+            gala: {
+                name: "LU Gala Egg S/P 6's Box",
+                company: "LU",
+                price: 159,
+                img: "pic/Biscuits/gala.webp",
 
             },
-            Wheatable : {
-                name : "LU Wheatable Sugar Free F/P",
-                company : "LU",
-                price : 80,
-                img : "",
+            Wheatable: {
+                name: "LU Wheatable Sugar Free F/P",
+                company: "LU",
+                price: 80,
+                img: "pic/Biscuits/wheatable.webp",
             },
-            Wispy : {
-                name : "Hilal Wispy Wafer Rolls ChocoVanilla 12's Box",
-                company : "Hilal",
-                price : 179,
-                img : "",
+            Wispy: {
+                name: "Hilal Wispy Wafer Rolls ChocoVanilla 12's Box",
+                company: "Hilal",
+                price: 179,
+                img: "pic/Biscuits/wispy.webp",
             },
         },
         cake: {
-            marbleCake : {},
-            bakeTimePlain : {},
-            
+            marbleCake: {
+                name: "Hilal Bake Time Marble Cake Slices 6's Box",
+                company: "Hilal",
+                price: 198,
+                img: "pic/cake/marble.webp",
+            },
+            bakeTimePlain: {
+                name: "Hilal Bake Time Plain Cake Slices 6's Box",
+                company: "Hilal",
+                price: 198,
+                img: "pic/cake/bakeTimePlain.webp",
+            },
+            cupCake: {
+                name: "Hilal Cup Kake Birthday 12's Box",
+                company: "Hilal",
+                price: 205,
+                img: "pic/cake/cupCake.webp",
+            },
+            chocoCake: {
+                name: "Hilal Chocokake Tripple Chocolate 12'S Box",
+                company: "Hilal",
+                price: 159,
+                img: "pic/cake/chocoCake.webp",
+            },
+            twinCake: {
+                name: "Euro Twin Banana Cake 32g",
+                company: "Euro Cake",
+                price: 72,
+                img: "pic/cake/twinCake.webp",
+            },
+        },
+    },
+    fresh: {
+        freshVegetable: {
+            onions: {
+                name: "Fresh Onions 500g",
+                company: "Fresh Stock",
+                price: 35,
+                img: "pic/freshVagetable/onions.webp",
+            },
+            garlic: {
+                name: "Fresh Garlic (Desi) 250g",
+                company: "Fresh Stock",
+                price: 59,
+                img: "pic/freshVagetable/garlic.webp",
+            },
+            chillies: {
+                name: "Fresh Green Chillies (Sindh) 250g",
+                company: "Fresh Stock",
+                price: 19,
+                img: "pic/freshVagetable/chillies.webp",
+            },
+            carrot: {
+                name: "Fresh Carrot (Orange) 500g",
+                company: "Fresh Stock",
+                price: 99,
+                img: "pic/freshVagetable/carrot.webp",
+            },
+            Cauliflower: {
+                name: "Fresh Cauliflower 500g",
+                company: "Fresh Stock",
+                price: 39,
+                img: "pic/freshVagetable/Cauliflower.webp",
+            },
+        },
+        freshFruits: {
+            bananas: {
+                name: "Fresh Bananas (A-Grade) 500g",
+                company: "Fresh Stock",
+                price: 149,
+                img: "pic/Fruits/banana.webp",
+            },
 
         },
-        rusks: {},
+        meat: {
+            meat: {
+                name: "Veal Boneless 500g",
+                company: "Fresh Stock",
+                price: 539,
+                img: "pic/meat/meat.webp",
+            },
+        },
+        Poultry: {
+            chicken: {
+                name: "Chicken Boti Cut 500g",
+                company: "Fresh Stock",
+                price: 378,
+                img: "pic/chicken/chicken.webp",
+            },
+        },
+
     },
-    fresh: {},
-    beverages: {},
-    frozen: {},
-    pharmacy: {},
+    beverages: {
+        softDrink: {
+
+            redBull: {
+                name: "Red Bull Drink Can 250ml",
+                company: "Red Bull",
+                price: 365,
+                img: "pic/beverages/redbull.webp",
+            },
+
+            sprite: {
+                name: "Sprite Drink Can 250ml Local",
+                company: "Sprite",
+                price: 85,
+                img: "pic/beverages/sprite.webp",
+            },
+
+            slice: {
+                name: "Slice Fruit Drink Mango 355ml",
+                company: "Slice",
+                price: 55,
+                img: "pic/beverages/slice.webp",
+            },
+
+            fruita: {
+                name: "Nestle FV Sprk Fruit Drink Lime Can 250ml",
+                company: "Nestle",
+                price: 115,
+                img: "pic/beverages/fruita.webp",
+            },
+            haleeb: {
+                name: "Haleeb Fruit Drink Chaunsa Chaunsa 200ml",
+                company: "Haleeb",
+                price: 30,
+                img: "pic/beverages/haleeb.webp",
+            },
+        },
+        water: {
+            dasani: {
+                name: "Dasani Mineral Water 1500ml",
+                company: "Dasani",
+                price: 75,
+                img: "pic/beverages/dasani.webp",
+            },
+        },
+        milk: {
+
+            milo: {
+                name: "Milo Drinking Powder Pouch 300g",
+                company: "Milo",
+                price: 749,
+                img: "pic/beverages/milo.webp",
+            },
+        },
+
+    },
+    frozen: {
+        chicken : {
+            kofta : {
+                name : "Sabroso Saver Kofta 900g Poly Bag",
+                company : "Sabroso",
+                price : 625,
+                img : "pic/frozen/kofta.webp",
+            },
+
+        },
+        frenchFries : {
+            friesOrignal : {
+                name : "OPA FRIES ORGNL 2KG",
+                company : "Opa",
+                price : 1095,
+                img : "pic/frozen/fries.webp",
+            }
+        },
+        puri : {
+            paratha : {
+
+                name : "Dawn Lachha Paratha 4's 400g",
+                company : "Dawn",
+                price : 189,
+                img : "pic/frozen/paratha.webp",
+            },
+        },
+        pizza : {
+            dough : {
+                name : "Roy Qual Pizza Crust Medium",
+                company : "Roy Qual",
+                price : 95,
+                img : "pic/frozen/pizza.webp",
+            }
+        },
+    },
+    Beauty : {
+        oralCare : {
+            mouthWash : {
+                name : "Colgate Plax Mouth Wash Peppermint 250Ml",
+                company : "Colgate",
+                price : 400,
+                img : "pic/Beauty/mouthWash.webp",
+            },
+            brush : {
+                name : "Shield Tooth Brush Bamboo",
+                company : "Shield",
+                price : 164,
+                img : "pic/Beauty/toothbrush.webp",
+            },
+            past : {
+                name : "CloseUp Tooth Gel Deep Action Menthol Fresh 50ml",
+                company : "CloseUp",
+                price : 249,
+                img : "pic/Beauty/past.webp",
+            },
+        },
+        skinCare : {
+            honeyLotion : {
+                name : "Caresse Honey Lotion Pump Almond&Aloevera 320ml",
+                company : "Caresse",
+                price : 655,
+                img : "pic/Beauty/lotion.webp",
+            }
+        },
+        hairCare : {
+            hairBrush : {
+                name : "Maggie Hair Brush Art # 71-10-SHR",
+                company : "Maggie",
+                price : 739,
+                img : "pic/Beauty/hairBrush.webp",
+            }
+        },
+        hygiene : {
+            Sanitizer : {
+                name : "Delite Hand Sanitizer Apple 60Ml",
+                company : "Delite",
+                price : 115,
+                img : "pic/Beauty/Sanitizer.webp",
+            }
+        },
+        fragrances : {
+            bluOud : {
+                name : "Lattafa EDP Blue Oud 100ml",
+                company : "Lattafa",
+                price : 4500,
+                img : "pic/Beauty/blue.webp",
+            },
+            tagHim : {
+                name : "Armaf EDT Tag-Him Pour Homme 100ml",
+                company : "Armaf",
+                price : 4500,
+                img : "pic/Beauty/tagHim.webp",
+            }
+
+        },
+
+    },
 
 
 }
@@ -98,7 +333,7 @@ function allItems() {
                 mainPage.innerHTML += `
                 <div class="col">
                 <div class="card h-100 main-card">
-                    <img src="${ onlineStotre[main][cat][item].img }" class="card-img-top" alt="">
+                    <img src="${onlineStotre[main][cat][item].img}" class="card-img-top" alt="">
                     <div class="card-body">
                         <h5 class="card-title">${onlineStotre[main][cat][item].name}</h5>
                         <p class="card-text">${onlineStotre[main][cat][item].company}</p>
